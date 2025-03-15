d043ed7e7b107e586bab91c95303d5b7ada19974
Luis Rodriguez <rodriguez.luis020797@gmail.com>
2025-03-14 18:12:57 -0600

    Add navbar component

--- AMWebUI/src/app/app.component.ts
+++ AMWebUI/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { NavBarComponent } from './partials/nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterOutlet, HomeComponent, NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
