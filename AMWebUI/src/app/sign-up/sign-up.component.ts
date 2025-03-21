db1294fe96628cf69b90ca56e408ad0cd7276b7b
Luis Rodriguez <rodriguez.luis020797@gmail.com>
2025-03-21 15:01:25 -0600

    add sign up component

---
+++ AMWebUI/src/app/sign-up/sign-up.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'am-sign-up',
+  imports: [],
+  templateUrl: './sign-up.component.html',
+  styleUrl: './sign-up.component.css'
+})
+export class SignUpComponent {
+
+}
