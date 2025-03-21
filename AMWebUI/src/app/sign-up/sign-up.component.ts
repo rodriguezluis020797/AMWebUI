d6c870eb3546909d73898de86280fafac2f8e4be
Luis Rodriguez <rodriguez.luis020797@gmail.com>
2025-03-21 15:24:29 -0600

    add sign up html styling

--- AMWebUI/src/app/sign-up/sign-up.component.ts
+++ AMWebUI/src/app/sign-up/sign-up.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UserDTO } from '../../models/UserDTO';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'am-sign-up',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './sign-up.component.html',
-  styleUrl: './sign-up.component.css'
+  styleUrl: './sign-up.component.css',
 })
 export class SignUpComponent {
-
+  dto: UserDTO = new UserDTO();
+  confirmPassword: string = '';
+  disableSubmit: boolean = false;
+  submit() {}
 }
