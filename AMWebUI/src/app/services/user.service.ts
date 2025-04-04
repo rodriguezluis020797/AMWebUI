3ada317a977ad0bfc61fa577be21e56404fbcc58
Luis Rodriguez <rodriguez.luis020797@gmail.com>
2025-04-04 17:26:12 -0600

    Add a pipe to signUpAsync()

--- AMWebUI/src/app/services/user.service.ts
+++ AMWebUI/src/app/services/user.service.ts
@@ -1,7 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { IUserDTO } from '../../models/UserDTO';
-import { Observable } from 'rxjs';
+import { catchError, Observable } from 'rxjs';
+import { RequestStatusEnum } from '../../models/Enums';
 
 @Injectable({
   providedIn: 'root',
@@ -10,6 +11,22 @@ export class UserService {
   constructor(private http: HttpClient) {}
 
   signupAsync(user: IUserDTO): Observable<IUserDTO> {
-    return this.http.post<IUserDTO>('/api/User/CreateUser', user);
+    return this.http.post<IUserDTO>('/api/User/CreateUser', user).pipe(
+      catchError((error: HttpErrorResponse) => {
+        const defaultUser: IUserDTO = {
+          userId: '',
+          firstName: '',
+          middleName: null,
+          lastName: '',
+          eMail: '',
+          password: '',
+          jwtToken: '',
+          requestStatus: RequestStatusEnum.Error,
+          errorMessage: '',
+          isTempPassword: false,
+        };
+        return of(defaultUser);
+      })
+    );
   }
 }
