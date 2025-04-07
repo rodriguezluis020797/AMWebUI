f1681687421b40bd51093478687c61e1e6fcb14e
Luis Rodriguez <rodriguez.luis020797@gmail.com>
2025-04-06 18:27:51 -0600

    Add directive for every click to ping. Will replace the interceptor.

---
+++ AMWebUI/src/app/directives/identity-ping.directive.spec.ts
@@ -0,0 +1,8 @@
+import { IdentityPingDirective } from './identity-ping.directive';
+
+describe('IdentityPingDirective', () => {
+  it('should create an instance', () => {
+    const directive = new IdentityPingDirective();
+    expect(directive).toBeTruthy();
+  });
+});
