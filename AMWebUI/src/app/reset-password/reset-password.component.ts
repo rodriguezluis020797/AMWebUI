9e1e1acdb17ae0c0887034af2f8106e1b7e1c772
Luis Rodriguez <rodriguez.luis020797@gmail.com>
2025-04-26 18:01:43 -0600

    Generate reset password

---
+++ AMWebUI/src/app/reset-password/reset-password.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'am-reset-password',
+  imports: [],
+  templateUrl: './reset-password.component.html',
+  styleUrl: './reset-password.component.css'
+})
+export class ResetPasswordComponent {
+
+}
