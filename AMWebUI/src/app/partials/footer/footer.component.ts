a53ec61f3141ff1230804041e4d7e4276a2cbbd3
Luis Rodriguez <rodriguez.luis020797@gmail.com>
2025-06-04 17:05:30 -0600

    update footer.. will always stay at the bottom...

--- AMWebUI/src/app/partials/footer/footer.component.ts
+++ AMWebUI/src/app/partials/footer/footer.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 
 @Component({
   standalone: true,
   selector: 'am-footer',
-  imports: [RouterLink],
+  imports: [],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css',
 })
