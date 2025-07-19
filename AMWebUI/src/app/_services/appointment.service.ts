d79ffc8d41a741740ac51b262bd35f12af2ef137
Luis Rodriguez <rodriguez.luis020797@gmail.com>
2025-07-19 02:39:00 -0700

    Update appointment.service.ts

--- AMWebUI/src/app/_services/appointment.service.ts
+++ AMWebUI/src/app/_services/appointment.service.ts
@@ -4,6 +4,7 @@ import { AppointmentDTO } from '../models/AppointmentDTO';
 import { catchError, Observable, of } from 'rxjs';
 import { Router } from '@angular/router';
 import { CurrentStateService } from './current-state.service';
+import { AppointmentRequestDTO } from '../models/AppointmentRequestDTO';
 
 @Injectable({
   providedIn: 'root'
@@ -15,6 +16,21 @@ export class AppointmentService {
     private router: Router,
     private currentStateService: CurrentStateService
   ) { }
+
+  appointmentRequestAsync(dto: AppointmentRequestDTO): Observable<AppointmentRequestDTO | null> {
+    return this.http.post<AppointmentRequestDTO>('/api/Appointment/AppointmentRequest', dto)
+      .pipe(
+        catchError((error: HttpErrorResponse) => {
+          if (error.status === 401) {
+            this.currentStateService.loggedInSubject.next(false);
+            this.router.navigate(['/unauthorized']);
+          } else {
+            this.router.navigate(['/error']);
+          }
+          return of(null);
+        })
+      );
+  }
 
   createAppointmentAsync(dto: AppointmentDTO): Observable<AppointmentDTO | null> {
     return this.http.post<AppointmentDTO>('/api/Appointment/CreateAppointment', dto, {
